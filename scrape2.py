1c7881cfcf502a94bb8abea536e5c5b1dda496ac
Andrew Godfrey <agodfrey3@fordham.edu>
2016-08-09 19:30:32 -0400

    Efficiency, Documentation, and Style Fixed

--- scrape2.py
+++ scrape2.py
@@ -1,87 +1,64 @@
+#Andrew Godfrey
+#August 9, 2016
+#RS Item Scraper Version 0.0.3
+
 import json
 import urllib2
 import time
 
-
+#Used for Bug testing
 successful = 0
 failed404  = 0
 failedVal  = 0
+
+#Used as ItemID iterator
 x          = 0
 
-f = open('itemFile_8_7_2016.txt'  , 'w')
-w = open('ID_used_8_7_2016.txt'   , 'w')
-q = open('ID_unused_8_7_2016.txt' , 'w')
-
-
-def rs_scrape( x , successful , failed404 , failedVal ):
-        time.sleep(.4)
-        try:
-                data = json.load(urllib2.urlopen('http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=' + str(x)))
-                successful += 1
-		item = data['item']['name']
-		f.write(str(x) + '-' + str(item) + '\n')
-		w.write(str(x) + '\n')
-		x = x + 1
-		print "Found item: " + str(item)
-		do_scrape( x , successful , failed404, failedVal)
-
-        except urllib2.HTTPError:
-                failed404 += 1
-		q.write(str(x) + '\n')
-		x = x +  1
-		print "ID does not correlate to a tradeable item"
-        	do_scrape( x , successful , failed404, failedVal)
-		
-	except ValueError:
-		time.sleep(2)
-                do_scrape( x , successful, failed404, failedVal)
-                failedVal += 1
-                print "Value Error"
-        
+#Creates or edits text files that will contain item information
+items = open('itemFile_8_7_2016.txt'  , 'w')
+used_items = open('ID_used_8_7_2016.txt'   , 'w')
+unused_items = open('ID_unused_8_7_2016.txt' , 'w')
 
 def do_scrape( x , successful, failed404, failedVal):
-	while (x < 1000):
-        	print "Looking for id: " + str(x)
-        	try:
-        	        data = json.load(urllib2.urlopen('http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=' + str(x)))
-        	        successful += 1
-			item = data['item']['name']
-			f.write(str(x) + '-' + str(item) + '\n')
-			w.write(str(x) + '\n')
-			x = x + 1
-        	        print "Found item: " + str(item)		
-        	except urllib2.HTTPError:
-        	        failed404 += 1
-			q.write(str(x) + '\n')
-			x = x + 1
-        	        print "ID does not correlate to a tradeable item"
-        	except ValueError:
-			time.sleep(2)
-			rs_scrape( x, successful, failed404, failedVal)
-        	        
-	print "Num successful: " + str(successful)
-	print "    404 Errors: " + str(failed404)
-	print "   Value Erros: " + str(failedVal)       	        
+        #Main While loop: Checks Item Ids from 0 to X
+        while (x <= 50):
+                print "Looking for id: " + str(x)
+                time.sleep(0.5)
+                
+                #checks to see if item is tradeable
+                try:
+                        data = json.load(urllib2.urlopen('http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=' + str(x)))
+                        successful += 1
+                        item = data['item']['name']
+                        items.write(str(x) + '-' + str(item) + '\n')
+                        used_items.write(str(x) + '\n')
+                        x += 1
+                        print "Found item: " + str(item)
+                #If item is not tradeable, this runs
+                except urllib2.HTTPError:
+                        failed404 += 1
+                        unused_items.write(str(x) + '\n')
+                        x += 1
+                        print "ID does not correlate to a tradeable item"
 
+                #Notifies if page request is being blocked
+                except ValueError:
+                        time.sleep(2)
+
+        #Looks for bugs caused from sending too many page requests
+        print "Num successful: " + str(successful)
+        print "    404 Errors: " + str(failed404)
+        print "   Value Erros: " + str(failedVal)
+
+#Times the operation for efficiency testing
 start = time.time()
 do_scrape(x , successful, failed404, failedVal)
 end = time.time()
 
-print end - start 
+print end - start
 
-f.close()
-w.close()
-q.close()
-
-
-
-
-
-
-
-
-
-
-
-
+#closes files
+items.close()
+used_items.close()
+unused_items.close()
 
